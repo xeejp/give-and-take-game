0eb1cf46045d585742ad59b6a964e79242edf625
NotFounds <m134_warrock_rpg7@yahoo.com>
2016-09-30 14:14:01 +0900

    Use react-throttle-render

--- host/Users.js
+++ host/Users.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import throttle from 'react-throttle-render'
 
 import { Card, CardHeader, CardText } from 'material-ui/Card'
 import { getRoleName, getStateName } from '../util/index.js'
@@ -100,4 +101,4 @@ const Users = ({ pairs, participants, game_round, openParticipantPage, participa
   </div>
 )
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users)
+export default connect(mapStateToProps, mapDispatchToProps)(throttle(Users, 200))
